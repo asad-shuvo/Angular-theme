076647da87e5f967d856caa247f4daec62cd184e
asadshuvo <asaduzzaman.shuvo@selise.ch>
2020-10-01 19:38:35 +0600

    section 3 added

--- src/app/section3/section3.component.ts
+++ src/app/section3/section3.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-section3',
   templateUrl: './section3.component.html',
-  styleUrls: ['./section3.component.css']
+  styleUrls: ['./section3.component.scss']
 })
 export class Section3Component implements OnInit {
-
+  @Input('theme') theme: any;
   constructor() { }
 
   ngOnInit(): void {
